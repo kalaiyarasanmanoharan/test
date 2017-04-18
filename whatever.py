1f686b9a662641f3a86599a4f1c9994d309e65a1
Your Name <you@example.com>
2017-04-18 07:33:05 -0700

    added new files

---
+++ whatever.py
@@ -0,0 +1,16 @@
+
+def func1():
+    print "hello whatever printed from func1"
+
+def func2(arg1='Kalai', arg2='Sindhu', arg3='Malathi'):
+    print arg1,arg2,arg2
+    print "printed from func2"
+
+def func3():
+    print "empty func3 exectuted from whatever.py"
+
+def main():
+     print "Hello whatever.py main function is executed"
+
+if __name__ == "__main__":
+    main()
---
+++ world.py
@@ -0,0 +1,16 @@
+
+def func1():
+    print "hello world printed from func1"
+
+def func2(arg1='Chennai', arg2='Coimbatore', arg3='Thanjavur'):
+    print arg1,arg2,arg2
+    print "printed from func2"
+
+def func3():
+    print "empty func3 exectured"
+
+def main():
+    print "Hello world.py main function is executed"
+
+if __name__ == "__main__":
+    main()
